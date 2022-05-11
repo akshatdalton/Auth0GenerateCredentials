f44f8a868b4733419dfd9493fbbab93a9dc327b8
akshatdalton <akshat.dak@students.iiit.ac.in>
2022-05-11 12:47:07 +0530

    Add disk-based caching.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime, timedelta
+from diskcache import Cache
 
 ################# USER INPUT #################
 
@@ -15,18 +15,15 @@ GRANT_TYPE = ""
 
 class AcessTokenGenerator(object):
     def __init__(self) -> None:
-        self._access_token, self._expiry_date = None, None
-        self._generate_access_token()
+        self._cache = Cache()
 
     def get_access_token(self):
-        if self.is_expired():
-            self._generate_access_token()
-        return self._access_token
+        with Cache(self._cache.directory) as reference:
+            if "access_token" not in reference:
+                self._generate_access_token(reference)
+            return reference.get("access_token")
 
-    def is_expired(self):
-        return self._expiry_date <= datetime.now()
-
-    def _generate_access_token(self):
+    def _generate_access_token(self, reference):
         data = {
             "client_id": CLIENT_ID,
             "client_secret": CLIENT_SECRET,
@@ -37,13 +34,11 @@ class AcessTokenGenerator(object):
         res = requests.post(f"https://{DOMAIN}/{ACCESS_TOKEN_API_PATH}", data=data)
         if res.ok:
             json_res = res.json()
-            self._access_token = json_res["access_token"]
-            self._update_expiry_date(json_res["expires_in"])
+            reference.set(
+                "access_token", json_res["access_token"], expire=json_res["expires_in"]
+            )
         else:
             print("Some error occured")
-
-    def _update_expiry_date(self, expires_in: int):
-        self._expiry_date = datetime.now() + timedelta(0, expires_in)
 
 
 ############### OUTPUT ###################
