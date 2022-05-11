243d6481988225cbd04d54fd029624bd64a088f7
akshatdalton <akshat.dak@students.iiit.ac.in>
2022-05-11 10:58:03 +0530

    Add caching to fetch `access_token`.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timedelta
 
 ################# USER INPUT #################
 
@@ -6,29 +7,49 @@ DOMAIN = ""
 ACCESS_TOKEN_API_PATH = ""
 CLIENT_ID = ""
 CLIENT_SECRET = ""
-AUDIENCE = ""     # aka `identifier`
+AUDIENCE = ""  # aka `identifier`
 GRANT_TYPE = ""
 
 ##############################################
 
 
-data = {
-    "client_id": CLIENT_ID,
-    "client_secret": CLIENT_SECRET,
-    "audience": AUDIENCE,
-    "grant_type": GRANT_TYPE,
-}
+class AcessTokenGenerator(object):
+    def __init__(self) -> None:
+        self._access_token, self._expiry_date = None, None
+        self._generate_access_token()
+
+    def get_access_token(self):
+        if self.is_expired():
+            self._generate_access_token()
+        return self._access_token
+
+    def is_expired(self):
+        return self._expiry_date <= datetime.now()
+
+    def _generate_access_token(self):
+        data = {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "audience": AUDIENCE,
+            "grant_type": GRANT_TYPE,
+        }
+
+        res = requests.post(f"https://{DOMAIN}/{ACCESS_TOKEN_API_PATH}", data=data)
+        if res.ok:
+            json_res = res.json()
+            self._access_token = json_res["access_token"]
+            self._update_expiry_date(json_res["expires_in"])
+        else:
+            print("Some error occured")
+
+    def _update_expiry_date(self, expires_in: int):
+        self._expiry_date = datetime.now() + timedelta(0, expires_in)
 
 
-res = requests.post(f"https://{DOMAIN}/{ACCESS_TOKEN_API_PATH}", data=data)
+############### OUTPUT ###################
+############ ACCESS TOKEN ################
 
+generator = AcessTokenGenerator()
+print(generator.get_access_token())
 
-################## OUTPUT ####################
-############### ACCESS TOKEN #################
-
-if res.ok:
-    print(res.json())
-else:
-    print("Some error occured")
-
-###############################################
+##########################################
